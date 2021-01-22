52fc61065ff9225d2b086690bb5f4bea754a1cd5
txfs19260817 <duola865@gmail.com>
2021-01-22 14:18:47 +0800

    linked list finished - add 1474

---
+++ leetcode/1474.Delete-N-Nodes-After-M-Nodes-of-a-Linked-List/1474. Delete N Nodes After M Nodes of a Linked List.go
@@ -0,0 +1,33 @@
+package _474_Delete_N_Nodes_After_M_Nodes_of_a_Linked_List
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteNodes(head *ListNode, m int, n int) *ListNode {
+	p := head
+	for i := 0; p != nil && i < m-1; i++ {
+		p = p.Next
+	}
+	if p == nil {
+		return head
+	}
+	q := p.Next
+	p.Next = nil
+	for i := 0; q != nil && i < n-1; i++ {
+		q = q.Next
+	}
+	if q == nil {
+		return head
+	}
+	nextHead := q.Next
+	p.Next = deleteNodes(nextHead, m, n)
+	return head
+}
+
+/*
+[1,2,3,4,5,6,7,8,9,10,11]
+1
+3
+*/
