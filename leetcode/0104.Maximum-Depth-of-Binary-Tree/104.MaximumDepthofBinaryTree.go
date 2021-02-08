ffbc846b8c478883fdcb0a140579669244c8b509
txfs19260817 <zeminjiang@mail.ru>
2021-02-08 23:43:33 +0800

    Tree WIP0.5

---
+++ leetcode/0104.Maximum-Depth-of-Binary-Tree/104.MaximumDepthofBinaryTree.go
@@ -0,0 +1,21 @@
+package _104_Maximum_Depth_of_Binary_Tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
