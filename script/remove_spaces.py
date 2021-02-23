e52c008ff0318c2b616d7f2da5dac07d8a5d9976
txfs19260817 <duola865@gmail.com>
2021-02-23 15:42:30 +0800

    fixed py

---
+++ script/remove_spaces.py
@@ -0,0 +1,10 @@
+import os
+
+def remove_spaces(path):
+    for curDir, dirs, files in os.walk(path):
+        for f in files:
+            filename = os.path.join(curDir, f)
+            os.rename(os.path.join(path, filename), os.path.join(path, filename.replace(' ', '')))
+
+if __name__ == "__main__":
+    remove_spaces("../leetcode") # TODO: input path from console
