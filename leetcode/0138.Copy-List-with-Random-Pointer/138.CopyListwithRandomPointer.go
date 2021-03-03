3f39a7a5c0882ab3d3446694aa0b1ba30d2f5972
txfs19260817 <duola865@gmail.com>
2021-03-03 22:53:38 +0800

    added 138

---
+++ leetcode/0138.Copy-List-with-Random-Pointer/138.CopyListwithRandomPointer.go
@@ -0,0 +1,31 @@
+package _138_Copy_List_with_Random_Pointer
+
+type Node struct {
+	Val    int
+	Next   *Node
+	Random *Node
+}
+
+func copyRandomList(head *Node) *Node {
+	if head == nil {
+		return nil
+	}
+	for p := head; p != nil; {
+		newNode := &Node{p.Val, p.Next, p.Random}
+		p.Next = newNode
+		p = newNode.Next
+	}
+	for p := head; p != nil; p = p.Next.Next {
+		if p.Random != nil {
+			p.Next.Random = p.Random.Next
+		}
+	}
+	dummy := &Node{}
+	q := dummy
+	for p := head; p != nil; p = p.Next {
+		q.Next = p.Next
+		q = q.Next
+		p.Next = q.Next
+	}
+	return dummy.Next
+}
