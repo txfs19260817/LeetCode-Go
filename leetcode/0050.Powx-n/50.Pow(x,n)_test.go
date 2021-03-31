ee10ee02bdbe9cc0c4b3dd5b2f4e0abdd803de2c
txfs19260817 <duola865@gmail.com>
2021-03-31 11:36:56 +0800

    fixed test file 50

--- leetcode/0050.Powx-n/50.Pow(x,n)_test.go
+++ leetcode/0050.Powx-n/50.Pow(x,n)_test.go
@@ -17,11 +17,19 @@ func Test_myPow(t *testing.T) {
 			args: args{34.00515, -3},
 			want: 0.00003,
 		},
+		{
+			name: "3, 3",
+			args: args{3, 3},
+			want: 27,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := myPow(tt.args.x, tt.args.n); got > tt.want {
 				t.Errorf("myPow() = %v, want %v", got, tt.want)
+			}
+			if got := myPow1(tt.args.x, tt.args.n); got > tt.want {
+				t.Errorf("myPow1() = %v, want %v", got, tt.want)
 			}
 		})
 	}
