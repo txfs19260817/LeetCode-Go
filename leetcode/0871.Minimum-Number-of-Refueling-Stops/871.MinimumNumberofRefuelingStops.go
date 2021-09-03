de3ec1e7f016cbc6c2ed613009000787ef53704b
v-zishuoding <v-zishuoding@microsoft.com>
2021-09-03 11:27:11 +0800

    871

---
+++ leetcode/0871.Minimum-Number-of-Refueling-Stops/871.MinimumNumberofRefuelingStops.go
@@ -0,0 +1,75 @@
+package _871_Minimum_Number_of_Refueling_Stops
+
+import "container/heap"
+
+func minRefuelStops(target int, startFuel int, stations [][]int) int {
+	dp := make([]int, len(stations)+1)
+	dp[0] = startFuel
+	for i := 0; i < len(stations); i++ {
+		for t := i; t >= 0; t-- {
+			if dp[t] >= stations[i][0] {
+				dp[t+1] = max(dp[t+1], dp[t]+stations[i][1])
+			}
+		}
+	}
+	for i, n := range dp {
+		if n >= target {
+			return i
+		}
+	}
+	return -1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func minRefuelStops2(target int, startFuel int, stations [][]int) int {
+	var ans, prev int
+	var h intHeap
+	stations = append(stations, []int{target, 0})
+	for _, station := range stations {
+		location, capacity := station[0], station[1]
+		startFuel -= location - prev // `startFuel` represents remaining fuel in tank
+		for len(h) > 0 && startFuel < 0 {
+			startFuel += heap.Pop(&h).(int)
+			ans++
+		}
+		if startFuel < 0 {
+			return -1
+		}
+		if location == target {
+			break
+		}
+		heap.Push(&h, capacity)
+		prev = location
+	}
+	return ans
+}
+
+type intHeap []int
+
+func (h intHeap) Len() int {
+	return len(h)
+}
+
+func (h intHeap) Less(i, j int) bool {
+	return h[i] > h[j]
+}
+
+func (h intHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *intHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *intHeap) Pop() interface{} {
+	x := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return x
+}
---
+++ leetcode/0871.Minimum-Number-of-Refueling-Stops/871.MinimumNumberofRefuelingStops_test.go
@@ -0,0 +1,47 @@
+package _871_Minimum_Number_of_Refueling_Stops
+
+import "testing"
+
+func Test_minRefuelStops(t *testing.T) {
+	type args struct {
+		target    int
+		startFuel int
+		stations  [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "target = 1, startFuel = 1, stations = []",
+			args: args{1, 1, [][]int{}},
+			want: 0,
+		},
+		{
+			name: "target = 100, startFuel = 1, stations = [[10,100]]",
+			args: args{100, 1, [][]int{{10, 100}}},
+			want: -1,
+		},
+		{
+			name: "target = 100, startFuel = 10, stations = [[10,60],[20,30],[30,30],[60,40]]",
+			args: args{100, 10, [][]int{{10, 60}, {20, 30}, {30, 30}, {60, 40}}},
+			want: 2,
+		},
+		{
+			name: "target = 100, startFuel = 50, stations = [[25,25],[50,50]]",
+			args: args{100, 50, [][]int{{25,25}, {50,50}}},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minRefuelStops(tt.args.target, tt.args.startFuel, tt.args.stations); got != tt.want {
+				t.Errorf("minRefuelStops() = %v, want %v", got, tt.want)
+			}
+			if got := minRefuelStops2(tt.args.target, tt.args.startFuel, tt.args.stations); got != tt.want {
+				t.Errorf("minRefuelStops2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
