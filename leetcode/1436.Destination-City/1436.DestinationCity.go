a2af191c52c11dc45f6c20c5d81d6fbaa389df1a
txfs19260817 <duola865@gmail.com>
2021-10-01 18:14:33 +0800

    1436

---
+++ leetcode/1436.Destination-City/1436.DestinationCity.go
@@ -0,0 +1,15 @@
+package _436_Destination_City
+
+func destCity(paths [][]string) string {
+	ans, m := "", map[string]bool{}
+	for _, path := range paths {
+		m[path[0]] = true
+	}
+	for _, path := range paths {
+		if !m[path[1]] {
+			ans = path[1]
+			break
+		}
+	}
+	return ans
+}
---
+++ leetcode/1436.Destination-City/1436.DestinationCity_test.go
@@ -0,0 +1,27 @@
+package _436_Destination_City
+
+import "testing"
+
+func Test_destCity(t *testing.T) {
+	type args struct {
+		paths [][]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: `[["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]`,
+			args: args{[][]string{{"London","New York"},{"New York","Lima"},{"Lima","Sao Paulo"}}},
+			want: "Sao Paulo",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := destCity(tt.args.paths); got != tt.want {
+				t.Errorf("destCity() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
