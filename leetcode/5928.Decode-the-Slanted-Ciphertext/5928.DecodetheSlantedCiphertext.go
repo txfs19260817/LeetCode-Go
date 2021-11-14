9bbffb0f5a4cae3769ed19e58355f49e1791f7ae
txfs19260817 <duola865@gmail.com>
2021-11-14 12:54:50 +0800

    update 5928

--- leetcode/5928.Decode-the-Slanted-Ciphertext/5928.DecodetheSlantedCiphertext.go
+++ leetcode/5928.Decode-the-Slanted-Ciphertext/5928.DecodetheSlantedCiphertext.go
@@ -3,17 +3,15 @@ package _928_Decode_the_Slanted_Ciphertext
 import "strings"
 
 func decodeCiphertext(encodedText string, rows int) string {
-	ans, cols := make([]byte, 0, len(encodedText)), len(encodedText)/rows
-	rowPtrs := make([]int, rows)
-	for i := range rowPtrs {
-		rowPtrs[i] = i * (cols + 1)
-	}
-	for c := 0; c < cols; c++ {
-		for i, ptr := range rowPtrs {
-			if ptr < len(encodedText) && ptr < (i+1)*(cols+1) {
-				ans = append(ans, encodedText[ptr])
-				rowPtrs[i]++
-			}
+	ans := make([]byte, 0, len(encodedText))
+	for i, j, k, cols := 0, 0, 0, len(encodedText)/rows; j < cols; {
+		ans = append(ans, encodedText[i*cols+j])
+		i++
+		j++
+		if i == rows || j == cols { // reached edge, move to the next diag
+			k++
+			j = k
+			i = 0
 		}
 	}
 	return strings.TrimRight(string(ans), " ")
