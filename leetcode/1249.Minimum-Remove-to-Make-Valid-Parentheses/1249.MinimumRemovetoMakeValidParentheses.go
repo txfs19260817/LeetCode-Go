34ece3c012b825399a703ae9cf30a62153b31e99
v-zishuoding <v-zishuoding@microsoft.com>
2021-12-08 17:49:35 +0800

    1249

---
+++ leetcode/1249.Minimum-Remove-to-Make-Valid-Parentheses/1249.MinimumRemovetoMakeValidParentheses.go
@@ -0,0 +1,29 @@
+package _249_Minimum_Remove_to_Make_Valid_Parentheses
+
+func minRemoveToMakeValid(s string) string {
+	chars, balance, left := make([]rune, 0, len(s)), 0, 0
+	for _, c := range s { // remove invalid ')'
+		if c == '(' {
+			balance++
+			left++
+		} else if c == ')' {
+			if balance == 0 {
+				continue
+			}
+			balance--
+		}
+		chars = append(chars, c)
+	}
+	left -= balance // redundant '('
+	ans := make([]rune, 0, len(chars))
+	for i := 0; i < len(chars); i++ {
+		if chars[i] == '(' {
+			left--
+			if left < 0 { // remove rightmost '('
+				continue
+			}
+		}
+		ans = append(ans, chars[i])
+	}
+	return string(ans)
+}
---
+++ leetcode/1249.Minimum-Remove-to-Make-Valid-Parentheses/1249.MinimumRemovetoMakeValidParentheses_test.go
@@ -0,0 +1,37 @@
+package _249_Minimum_Remove_to_Make_Valid_Parentheses
+
+import "testing"
+
+func Test_minRemoveToMakeValid(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "))((",
+			args: args{"))(("},
+			want: "",
+		},
+		{
+			name: "())()(((",
+			args: args{"())()((("},
+			want: "()()",
+		},
+		{
+			name: ")))t((u)",
+			args: args{")))t((u)"},
+			want: "t(u)",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minRemoveToMakeValid(tt.args.s); got != tt.want {
+				t.Errorf("minRemoveToMakeValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
