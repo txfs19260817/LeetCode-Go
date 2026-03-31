1f58dbd80b830672537ca552d04e3c5bbbb137ee
txfs19260817 <duola865@gmail.com>
2026-03-30 23:02:42 -0700

    g9

--- others/google/Simplify-Expression/solution.py
+++ others/google/Simplify-Expression/solution.py
@@ -1,6 +1,39 @@
-from typing import List, Optional
-
 class Solution:
     def simplifyExpression(self, expr: str) -> str:
-        pass
-        # TODO: Implement simplifyExpression logic
+        ans = []
+        sign, stack = 1, [1]
+
+        for ch in expr:
+            if ch == "+":
+                sign = 1
+            elif ch == "-":
+                sign = -1
+            elif ch == "(":
+                stack.append(stack[-1] * sign)
+                sign = 1
+            elif ch == ")":
+                stack.pop()
+            else:  # var
+                effective = stack[-1] * sign
+                if not ans:
+                    if effective == -1:
+                        ans.append('-')
+                else:
+                    ans.append('+' if effective == 1 else '-')
+                ans.append(ch)
+                sign = 1
+        return "".join(ans)
+
+
+if __name__ == "__main__":
+    solver = Solution()
+
+    assert solver.simplifyExpression("a-(b+c)") == "a-b-c"
+    assert solver.simplifyExpression("a-(-b-c)") == "a+b+c"
+    assert solver.simplifyExpression("(x+y)-z") == "x+y-z"
+
+    assert solver.simplifyExpression("a+b-c") == "a+b-c"
+    assert solver.simplifyExpression("-(a-b+c)") == "-a+b-c"
+    assert solver.simplifyExpression("a-(b-(c-d))") == "a-b+c-d"
+    assert solver.simplifyExpression("a-((b+c)-d)") == "a-b-c+d"
+    assert solver.simplifyExpression("((a))") == "a"
